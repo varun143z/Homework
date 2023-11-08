b55afe578453e2fc511f60425b55499f830f6930
varun143z <147702824+varun143z@users.noreply.github.com>
2023-11-08 09:53:50 +0530

    area of a circle.c

---
+++ area of a circle.c
@@ -0,0 +1,14 @@
+/*
+ * C program to find the area of a triangle using formula
+ */
+#include <stdio.h>
+void main()
+{
+    float base,height;
+    printf("Enter Base and Height: ");
+    scanf("%f %f",&base,&height);
+    float area = (base * height) / 2;
+ 
+    //Area with precision of 2 decimal places
+    printf("Area of Triangle is %0.2f",area);
+}
